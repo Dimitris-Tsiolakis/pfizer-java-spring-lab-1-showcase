c74fafc6c7afffe716acbdf168de19def95bf713
Ioannis Daniil <jondan97@gmail.com>
2022-10-18 12:26:48 +0300

    intial step finished

--- src/main/java/com/acme/eshop/Application.java
+++ src/main/java/com/acme/eshop/Application.java
@@ -1,7 +1,9 @@
 package com.acme.eshop;
 
+import org.springframework.boot.SpringApplication;
+
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/com/acme/eshop/bootstrap/MainRunner.java
@@ -0,0 +1,15 @@
+package com.acme.eshop.bootstrap;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MainRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
